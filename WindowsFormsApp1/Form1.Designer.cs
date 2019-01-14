85f9b327f72dfd81e1d61cb2f78730318fe81b23
zpc3467 <zpc3467@zpc.server.com>
2019-01-14 18:55:16 +0800

    上传

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class DeepDarkFantasy
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DeepDarkFantasy));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.doAnalyze = new System.Windows.Forms.Button();
+            this.boxFilter = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.boxUrl = new System.Windows.Forms.TextBox();
+            this.boxHtml = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(331, 410);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(340, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "删";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("楷体", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox2.ForeColor = System.Drawing.Color.Red;
+            this.richTextBox2.Location = new System.Drawing.Point(424, 3);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(337, 410);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 420);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "行数";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(424, 420);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "行数";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(340, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "<<<清空";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(340, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "复制";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(782, 467);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.richTextBox2);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(774, 441);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "去除名字";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.doAnalyze);
+            this.tabPage2.Controls.Add(this.boxFilter);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.boxUrl);
+            this.tabPage2.Controls.Add(this.boxHtml);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(774, 441);
+            this.tabPage2.TabIndex = 0;
+            this.tabPage2.Text = "区域代码";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // doAnalyze
+            // 
+            this.doAnalyze.Location = new System.Drawing.Point(688, 411);
+            this.doAnalyze.Name = "doAnalyze";
+            this.doAnalyze.Size = new System.Drawing.Size(75, 23);
+            this.doAnalyze.TabIndex = 5;
+            this.doAnalyze.Text = "执行解析";
+            this.doAnalyze.UseVisualStyleBackColor = true;
+            this.doAnalyze.Click += new System.EventHandler(this.doAnalyze_Click);
+            // 
+            // boxFilter
+            // 
+            this.boxFilter.Location = new System.Drawing.Point(382, 63);
+            this.boxFilter.Name = "boxFilter";
+            this.boxFilter.Size = new System.Drawing.Size(382, 21);
+            this.boxFilter.TabIndex = 4;
+            this.boxFilter.Text = "//table[@class=\'provincetable\']/tr[@class=\'provincetr\']/td/a";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(383, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "解析关键字";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(383, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "解析网址：";
+            // 
+            // boxUrl
+            // 
+            this.boxUrl.Location = new System.Drawing.Point(382, 19);
+            this.boxUrl.Name = "boxUrl";
+            this.boxUrl.Size = new System.Drawing.Size(382, 21);
+            this.boxUrl.TabIndex = 1;
+            this.boxUrl.Text = "index.html";
+            // 
+            // boxHtml
+            // 
+            this.boxHtml.Location = new System.Drawing.Point(3, 3);
+            this.boxHtml.Name = "boxHtml";
+            this.boxHtml.Size = new System.Drawing.Size(373, 435);
+            this.boxHtml.TabIndex = 0;
+            this.boxHtml.Text = "";
+            // 
+            // DeepDarkFantasy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 468);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DeepDarkFantasy";
+            this.Text = "DeepDarkFantasy";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button doAnalyze;
+        private System.Windows.Forms.TextBox boxFilter;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox boxUrl;
+        private System.Windows.Forms.RichTextBox boxHtml;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,159 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class DeepDarkFantasy : Form
+    {
+        public DeepDarkFantasy()
+        {
+            InitializeComponent();
+        }
+        #region tab1功能
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = "";
+            var arr = richTextBox1.Text.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in arr)
+            {
+                var index = item.IndexOf("（") > 0 ? item.IndexOf("（") : item.IndexOf("(");
+                if (index > 0)
+                {
+                    richTextBox2.Text += item.Substring(index) + "\r\n";
+                }
+                else
+                {
+                    richTextBox2.Text += item + "\r\n";
+                }
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            var arr = richTextBox1.Text.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            //label1.Text = "共" + arr.Length.ToString() + "行";
+            label1.Text = string.Format("共{0}行", arr.Length.ToString());
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            var arr = richTextBox2.Text.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            //label2.Text = "共" + arr.Length.ToString() + "行";
+            label2.Text = string.Format("共{0}行", arr.Length.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(richTextBox2.Text);
+        }
+        #endregion
+
+        #region tab2功能
+        HttpWebRequest httpReq;
+        HttpWebResponse httpResp;
+
+        string strBuff = "";
+        char[] cbuffer = new char[256];
+        int byteRead = 0;
+
+        //string filename = @"c:\log.txt";
+        ///定义写入流操作 
+        public string WriteStream(string url)
+        {
+            Uri httpURL = new Uri(url);
+
+            ///HttpWebRequest类继承于WebRequest，并没有自己的构造函数，需通过WebRequest的Creat方法 建立，并进行强制的类型转换 
+            httpReq = (HttpWebRequest)WebRequest.Create(httpURL);
+            ///通过HttpWebRequest的GetResponse()方法建立HttpWebResponse,强制类型转换
+
+            httpResp = (HttpWebResponse)httpReq.GetResponse();
+            ///GetResponseStream()方法获取HTTP响应的数据流,并尝试取得URL中所指定的网页内容
+
+            ///若成功取得网页的内容，则以System.IO.Stream形式返回，若失败则产生ProtoclViolationException错 误。在此正确的做法应将以下的代码放到一个try块中处理。这里简单处理 
+            Stream respStream = httpResp.GetResponseStream();
+
+            ///返回的内容是Stream形式的，所以可以利用StreamReader类获取GetResponseStream的内容，并以
+
+            ///StreamReader类的Read方法依次读取网页源程序代码每一行的内容，直至行尾（读取的编码格式：UTF8） 
+            StreamReader respStreamReader = new StreamReader(respStream, Encoding.Default);
+
+            byteRead = respStreamReader.Read(cbuffer, 0, 256);
+
+            while (byteRead != 0)
+            {
+                string strResp = new string(cbuffer, 0, byteRead);
+                strBuff = strBuff + strResp;
+                byteRead = respStreamReader.Read(cbuffer, 0, 256);
+            }
+
+            respStream.Close();
+            return strBuff;
+        }
+
+        //解析html
+        private string s1 = "//table[@class='provincetable']/tr[@class='provincetr']/td/a";
+        private string s2 = "//table[@class='citytable']/tr[@class='citytr']/td/a";
+        private string s3 = "//table[@class='countytable']/tr[@class='countytr']/td/a";
+        private string s4 = "//table[@class='towntable']/tr[@class='towntr']/td/a";
+        private string htmlResult = "";
+        public string AnalyzeHtml(string url, string selectString, int tag)
+        {
+            var html = @"http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2017/" + url;
+            HtmlWeb web = new HtmlWeb
+            {
+                OverrideEncoding = Encoding.GetEncoding("gb2312")
+            };
+            var htmlDoc = web.Load(html);
+            var htmlNodes = htmlDoc.DocumentNode.SelectNodes(selectString);
+            string nextUrl = "";
+            string code = "";
+            string name = "";
+            try
+            {
+                foreach (var node in htmlNodes)
+                {
+                    nextUrl = string.IsNullOrEmpty(node.Attributes["href"].Value) ? "" : node.Attributes["href"].Value;
+                    name = node.InnerHtml;
+                    htmlResult += htmlNodes.IndexOf(node) + " " + code + " " + name + "\r\n";
+                    if (tag < 1)
+                    {
+                        AnalyzeHtml(nextUrl, s2, ++tag);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                htmlResult = "参数错误";
+            }
+            return htmlResult;
+        }
+
+        private void doAnalyze_Click(object sender, EventArgs e)
+        {
+            var s1 = "//table[@class='provincetable']/tr[@class='provincetr']/td/a";
+            var s2 = "//table[@class='citytable']/tr[@class='citytr']/td/a";
+            var s3 = "//table[@class='countytable']/tr[@class='countytr']/td/a";
+            var s4 = "//table[@class='towntable']/tr[@class='towntr']/td/a";
+            boxHtml.Text = "";
+            boxHtml.Text = AnalyzeHtml(boxUrl.Text, s1, 0);
+        }
+        #endregion
+
+    }
+}
